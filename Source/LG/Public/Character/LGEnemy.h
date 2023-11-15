f829609bf9d66138e11b9d5d54c7b7ac565d2e19
chenhaoran@yottaGames <906467339@qq.com>
2023-11-15 23:12:17 +0800

    Add high light actor interface

--- Source/LG/Public/Character/LGEnemy.h
+++ Source/LG/Public/Character/LGEnemy.h
@@ -4,14 +4,17 @@
 
 #include "CoreMinimal.h"
 #include "Character/LGCharacterBase.h"
+#include "Interaction/EnemyInterface.h"
 #include "LGEnemy.generated.h"
 
 /**
  * 
  */
 UCLASS()
-class LG_API ALGEnemy : public ALGCharacterBase
+class LG_API ALGEnemy : public ALGCharacterBase, public IEnemyInterface
 {
 	GENERATED_BODY()
-	
+public:
+	virtual void HighLightActor() override;
+	virtual void UnHighLightActor() override;
 };
